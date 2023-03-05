5b2a826ee823ccf7569f4642f4f322b1d376904e
MacFJA <macfja@gmail.com>
2023-03-05 22:15:13 +0100

    Add Map

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,5 +11,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 - Install SvelteKit Skeleton (Vite)
 - Github Action to run quality check
+- Add Map visualisation with daylight
+- (dev) Add page to test map (`/map`)
 
 [unreleased]: https://github.com/jycouet/Fuzo/compare/main...HEAD
---
+++ src/routes/map/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import DayLightMap from '$lib/DayLightMap.svelte';
+	import { utcDate } from '$lib/time';
+
+	let width = 1024;
+	let height = 800;
+
+	$: console.log({ height, width });
+</script>
+
+<svelte:window bind:innerHeight={height} bind:innerWidth={width} />
+
+<DayLightMap date={utcDate()} height={height - 40} {width} />
+
+<style>
+	:global(html) {
+		padding: 0;
+		margin: 0;
+	}
+	:global(body) {
+		padding: 0;
+		margin: 0;
+	}
+</style>
